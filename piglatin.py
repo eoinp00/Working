5d7c67fffa6075f0e0814b0669dbec75cd28eb61
eoinp00 <eoinpayne@hotmail.com>
2015-10-26 15:19:40 +0000

    git init
    Found nice pythonic way of comparing input to determine if anything but
    letters are present

--- piglatin.py
+++ piglatin.py
@@ -1,35 +1,53 @@
 ##pig latin
 #idea is to find the first constanant group (letters before first vowel) remove from the start of the word, add to the end and ad "ay"
-#piglatin
 
-import re
+##IMPROVE TO TAKE MORE THAN 1 WORD
 
-def takeUserWord():
+
+def takeUserWord():     #validates user input
+    alpha = "abcdefghijklmnopqrstuvwxyz"
+    alphalist = []
+    for i in range (0,len(alpha)): # creating list of aplhabet, to allow list comparisons.
+        alphalist.append(alpha[i])
+    print (alphalist)
 
     while True:
+        alpha = "abcdefghijklmnopqrstuvwxyz"
+
         word = str.lower(input("Enter word: "))
+        while len(word) <= 2:
+            print ("Word must be longer than 2 letters")
+            break
+        if len(word) > 2:
+            wordoflist = []
+            for i in range (0, len(word)): ##turning word into list to explore comparing using 'any'
+                #if word[i] not in "abcdefghijklmnopqrstuvwxyz":
+                wordoflist.append(word[i])
+            print (wordoflist)
 
+            if all[wordoflist] not in alphalist: ## exploring if any with li
+                print ("illegal char")
 
+            #
+            # else:
+            #     return word
 
-        if not len(word) < 2 and re.match("[a-z]", word):
-            #print ("Word must be more than 2 and a-z only!")
-            return word
-
-
-            elif not all word in
-            #for i in range (0, len(word)):
-             #   if word[i] in "abcdefghijklmnopqrstuvwxyz":
-
-              #      return word
-
-
+    # while True:
+    #     word = str.lower(input("Enter word: "))
+    #     if any([i > 'z' or i < 'a' for i in word]):    #compares each character in word against an index of a-z
+    #         print ("Error: Contains illegal characters")
+    #     elif len(word) <=2:
+    #         print ("Word too short")
+    #     else:
+    #         return word
 
 
 def findfirstvowel(word): #checks if first letter of word is a vowel
     i = 0
     if word[i] not in "aeiou":
-        i +=1
-    return i  ##returns i = 0 if first letter was a vowel.
+        i += 1
+    return i  ##returns position of first vowel
+
 
 def translate (word):
     i = findfirstvowel(word)
@@ -37,17 +55,6 @@ def translate (word):
         return word + "yay"
     elif i != 0: ## modifies based on position of first vowel
         return word[i:]+word[:i] + "ay"
-
-    '''
-    cluster = word[: i] #+1?
-    core = word [i : ]
-    print ("translation for ", word, "is: ", core+cluster + "ay")
-    '''
-    ##or could just return the values neededreturn
-    # print ("translation for ", word, "is: ", word[i:]+word[:i] + "ay")
-    return word[i:]+word[:i] + "ay"
-
-
 
 
 def main():
